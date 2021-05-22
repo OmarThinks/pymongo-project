19a6cad5ac2db2bff1ce1dec49b218fd3644c267
Omar Magdy <contact.parmag@gmail.com>
2021-05-22 13:49:04 +0200

    pp.py:the test collection

--- app.py
+++ app.py
@@ -4,4 +4,18 @@ from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 
-#db = client.test_database
+db = client.test_database
+
+
+
+collection = db.test_collection
+
+print(collection)
+"""
+Collection(Database(
+MongoClient(host=['localhost:27017'], document_class=dict, 
+tz_aware=False, connect=True), 'test_database'), 'test_collection')
+"""
+
+
+
