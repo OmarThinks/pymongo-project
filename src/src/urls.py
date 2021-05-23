ac7b2187eb3ce62cfcc847e301e7ee69e987906e
Omar Magdy <contact.parmag@gmail.com>
2021-05-23 08:23:16 +0200

    urls.py:add:router with categories urls

--- src/src/urls.py
+++ src/src/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+
+
+#from products.views import ProductViewSet
+from categories.views import CategoryViewSet
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+#router.register(r'products', ProductViewSet)
+router.register(r'categories', CategoryViewSet)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include((router.urls))),
 ]
