9022bea301c1473eb982fe74877435e22afe72f3
Omar Magdy <contact.parmag@gmail.com>
2021-05-23 08:19:23 +0200

    settings.py:mod:database

--- src/src/settings.py
+++ src/src/settings.py
@@ -73,11 +73,19 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}"""
+
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'djongo',
+       'NAME': 'pymongo-project',
+   }
 }
 
 
