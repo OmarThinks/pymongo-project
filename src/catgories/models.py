891f1797706bf3b0b5bac8d4aaaed037d72f673b
Omar Magdy <contact.parmag@gmail.com>
2021-05-23 08:21:53 +0200

    categories:models:created model with serializer

--- src/catgories/models.py
+++ src/catgories/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from rest_framework import serializers
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    """parent = models.ForeignKey("self", null=True, 
+    	blank=True, on_delete = models.SET_NULL, 
+    	related_name='children')"""
+
+
+
+
+
+
+
+
+
+
+# Serializers define the API representation.
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
